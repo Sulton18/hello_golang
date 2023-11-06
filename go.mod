a8e2baa0cc8536fcdecbb3bd6c9b53a6d1b7b24e
sulton darmawan <sulton.darmawan@dwidasa.com>
2023-11-06 14:26:52 +0700

    update major

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Sulton18/hello_golang
+module github.com/Sulton18/hello_golang/v2
 
 go 1.13
--- hello-golang.go
+++ hello-golang.go
@@ -1,5 +1,5 @@
 package go_hello
 
-func HelloGo() string {
+func HelloGo(nama string) string {
 	return "hello world"
 }
